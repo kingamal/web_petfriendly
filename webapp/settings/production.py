d215b9799f3657b9cd26337e8f89d7bc535f37f0
Kinga <kinga.maleszewska@o2.pl>
2021-10-20 20:46:30 +0200

    Use postgres on heroku

--- webapp/settings/production.py
+++ webapp/settings/production.py
@@ -1,11 +1,9 @@
 from webapp.settings.base import *
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3', #TODO
-    }
+    'default': dj_database_url.config()
 }
