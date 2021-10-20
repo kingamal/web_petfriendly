b5d82bd6e44a781924ded62970fea66f3d5dd400
Kinga <kinga.maleszewska@o2.pl>
2021-10-20 21:20:08 +0200

    Add redirect to home

--- petfriendly/urls.py
+++ petfriendly/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.urls import path, include
 
 from . import views
@@ -5,6 +6,7 @@ from . import views
 urlpatterns = [
     path('searching', views.searching, name='searchingview'),
     path('home', views.home, name='homeview'),
+    path('', lambda r: HttpResponseRedirect('home')),
     path('onas', views.onas, name='onasview'),
     path('top10', views.top10, name='top10view'),
     # path('datebase', views.datebase, name='datebaseview'),
