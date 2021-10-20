27155f8b2ce22146904b94530bec28b5386966a1
Kinga <kinga.maleszewska@o2.pl>
2021-10-20 20:09:21 +0200

    Add profiles

---
+++ webapp/settings/production.py
@@ -0,0 +1,11 @@
+from webapp.settings.base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.sqlite3', #TODO
+    }
+}
